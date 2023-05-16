9c4f62f0904647ebca72159bd79ae21ed8bb3c50
SosthenesMS <sosthenesms@yahoo.com.br>
2023-05-16 15:18:49 -0300

    First Commit

---
+++ src/main/java/com/challenge/hotel/alura/HotelAluraApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.hotel.alura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelAluraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/hotel/alura/HotelAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.hotel.alura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
